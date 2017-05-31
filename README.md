eecbfd7976e65528f4e46e980edef748ce937ac7
Thomas Wang <dev@paced.me>
2017-06-01 03:04:01 +1000

    Add context

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# listenserv
+listenserv
+==========
+
 A Python Flask server that logs all requests to the server in custom database.
+
+Introduction
+------------
+
+We shouldn't need to create one of these every time we want to do a specific attack; we should just be able to clone, run, and go. A common tool we need is the ability to capture information sent via HTTP GET request to a host we own. Sometimes we want to be able to send information/files as well. This project allows for both with very little configuration.
+
+Installation
+------------
+
+You will need:
+
+-	python >= 2.7, >= 3.5
+-	flask
+-	flask-cors
+
+Then:
+
+```bash
+$ sudo python run.py
+```
+
+Server will be running on localhost:80.
+
+Considerations
+--------------
+
+You may want to edit the run.py file or add your static files to the static/ directory. Static serving files is enabled by default.
+
+Do not run this with a web server such as nginx; there are a few considerable security risks depending on the running version of Flask. Just use this as an example or during CTFs and competitions as a quick and dirty capture server.
--- server/run.py
+++ server/run.py
@@ -7,36 +7,51 @@ from flask import Flask, request, send_from_directory
 from flask_cors import CORS
 from w3lib.html import replace_entities
 
+# Run the Flask server and ensure cross-origin resource sharing is okay.
+
 app = Flask(__name__)
 CORS(app)
+
+# Set serve message on GET request.
+with open("message.txt") as f:
+    message = f.read().rstrip()
+
+# Serve the app to the root path.
 
 
 @app.route('/')
 def route():
-    """Get all GET and POST requests and dump them to logs."""
-    # Print, log, and return.
-    print(request.url)
+    """Get all GET requests and dump them to logs."""
+    # Unescape HTML and write to log.
     with open("cap.log", "a") as f:
         f.write(replace_entities(str(request.url)) + "\n")
 
+    # If we think a keylogger (param: 'c') is trying to get us information...
     with open("key.log", "a") as f:
         if "c" in request.args:
             keys = loads(replace_entities(request.args.get('c')))
 
+            # From the JSON list get only keys pressed. If impossible, stop.
             try:
                 keys = "".join(keys)
+                f.write(keys + '\n')
             except Exception:
                 pass
 
-            f.write(keys + '\n')
+    return message
 
-    return "WARNING: This site exists to demonstrate a 'capture server' for a penetration tester. Every GET request you send to it will be logged and recorded. Old logs will be deleted after some time, but information you send here is not safe. Use this site for educational purposes only! I am not responsible for any damages caused, as this site will be taken down as frequently as possible to reduce damages."
+# Comment below if you would not like to serve static content through the
+# static folder. Note that this can be a security vulnerability.
+#
+# e.g.: ./server/static/test.txt may be at http://localhost/test.txt.
 
 
-# @app.route('/<path:path>')
-# def staticserve(path):
-#     """Serve a file from your static directory."""
-#     return app.send_static_file(path)
+@app.route('/<path:path>')
+def staticserve(path):
+    """Serve a file from your static directory."""
+    return app.send_static_file(path)
+
 
 if __name__ == "__main__":
+    # Must be run with sudo/root.
     app.run(host='0.0.0.0', port=80)
